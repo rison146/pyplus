fa8bd242d9b5738de20f3cb656f48043d1812426
Harry Sitorus <herrison@spotify.com>
2020-02-02 19:35:52 -0800

    Added strings to print

--- test.py
+++ test.py
@@ -3,6 +3,12 @@
 from __future__ import print_function, unicode_literals
 
 my_str1 = 'whatever'
+my_str2 = 'something else'
+my_str3 = '''This is a string
+that spans
+multiple lines
+'''
 
 print(mystr1)
-
+print(mystr2)
+print(mystr3)
