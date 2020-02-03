b7244c4c953ff3a16b1c5e75e9e2e10ca636f5ba
Harry Sitorus <herrison@spotify.com>
2020-02-02 19:25:30 -0800

    Week 1 lab files

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/user/bin env python
+
+from __future__ import print_function, unicode_literals
+
+my_str1 = 'whatever'
+
+print(mystr1)
+
---
+++ test1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals
+
+ip_addr1 = '192.168.1.1' 
+port1 = 80
+ip_addr2 = '192.168.2.1'
+port2 = 443
+
+print(ip_addr1)
+print(ip_addr2)
+
+
